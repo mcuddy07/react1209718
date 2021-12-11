1adcbe08f1af72984c5bc3ef2330a0d070b1b0a2
mcuddy07 <mcuddy07@gmail.com>
2021-12-10 19:37:31 -0600

    "add some code" asdf

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>This is a paragraph<p></p>
+  </div>,
+  document.getElementById("root")
+);
